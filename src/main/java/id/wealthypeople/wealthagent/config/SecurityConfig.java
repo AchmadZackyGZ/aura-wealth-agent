d6cfed6ac3e422a293aa3a79d0a6d0d6fc2c2d9d
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-04-30 02:00:24 +0700

    security: allow vercel cors final

--- src/main/java/id/wealthypeople/wealthagent/config/SecurityConfig.java
+++ src/main/java/id/wealthypeople/wealthagent/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://aura-wealth-frontend.vercel.app")); 
+        configuration.setAllowedOrigins(List.of("http://localhost:3000",
+        "https://aura-wealth-frontend.vercel.app")); 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
